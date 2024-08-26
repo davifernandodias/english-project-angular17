82c44ef64027185dc360847e24f914eb660433c8
davi dias <davifernandosousadias18@gmail.com>
2024-08-26 20:42:55 -0300

    feat: creating login page

--- src/app/components/default-login-layout/default-login-layout.component.ts
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -1,16 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-default-login-layout',
   standalone: true,
   imports: [],
   templateUrl: './default-login-layout.component.html',
-  styleUrl: './default-login-layout.component.scss'
+  styleUrls: ['./default-login-layout.component.scss'] // Corrigido aqui
 })
 export class DefaultLoginLayoutComponent {
-
   @Input() title: string = "";
   @Input() primaryBtnText: string = "";
   @Input() secundaryBtnText: string = "";
+  @Input() disablePrimaryBtn: boolean = true;
+
+  @Output("submit") onSubmit = new EventEmitter();
+  submit() {
+    this.onSubmit.emit();
+  }
 
 }
