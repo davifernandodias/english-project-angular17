159d8bdcaaa33beb4df21634f4d7b5d0abf4c73a
davi dias <davifernandosousadias18@gmail.com>
2024-09-04 21:30:14 -0300

    refactor: update davi component, adding type js library

--- src/app/components/content/content-home/content-home.component.ts
+++ src/app/components/content/content-home/content-home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-content-home',
@@ -7,6 +8,30 @@ import { Component } from '@angular/core';
   templateUrl: './content-home.component.html',
   styleUrl: './content-home.component.scss'
 })
-export class ContentHomeComponent {
+export class ContentHomeComponent implements AfterViewInit {
+  @ViewChild('typedOutput') typedOutput!: ElementRef;
 
+  ngAfterViewInit(): void {
+    const options = {
+      strings: ['Welcome to School', 'English School'],
+      typeSpeed: 50,
+      backSpeed: 25,
+      backDelay: 1000,
+      startDelay: 500,
+      loop: true,
+      showCursor: false,
+      onStringTyped: () => {
+        this.adjustHeight();
+      }
+    };
+
+    const typed = new Typed(this.typedOutput.nativeElement, options);
+
+    this.adjustHeight();
+  }
+
+  private adjustHeight() {
+    const element = this.typedOutput.nativeElement;
+    element.style.height = `${element.scrollHeight}px`;
+  }
 }
