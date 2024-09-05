8e4d7c999ad3df51484eaff90272a63709864c97
davi dias <davifernandosousadias18@gmail.com>
2024-09-04 21:33:00 -0300

    feat: creating us components

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from "../../components/header/header.component";
 import { ContentHomeComponent } from "../../components/content/content-home/content-home.component";
 import { ContentMetadologiasComponent } from "../../components/content/content-metadologias/content-metadologias.component";
 import { ContentFeedbacksComponent } from "../../components/content/content-feedbacks/content-feedbacks.component";
+import { UsComponent } from "../../components/content/content-us/us.component";
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [FooterComponent, HeaderComponent, ContentHomeComponent, ContentMetadologiasComponent, ContentFeedbacksComponent],
+  imports: [FooterComponent, HeaderComponent, ContentHomeComponent, ContentMetadologiasComponent, ContentFeedbacksComponent, UsComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
