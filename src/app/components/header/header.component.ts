26cb63d740983534155a5ac6af51177c4b2c66a8
davi dias <davifernandosousadias18@gmail.com>
2024-09-04 21:31:38 -0300

    refactor: header update to better better designer

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 import { PrimaryInputComponent } from "../primary-input/primary-input.component";
 
 @Component({
@@ -9,7 +8,14 @@ import { PrimaryInputComponent } from "../primary-input/primary-input.component"
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
 
+  ngOnInit(): void {
+    // Adiciona a classe 'loaded' ao elemento navbar após um breve atraso
+    setTimeout(() => {
+      this.renderer.addClass(this.el.nativeElement.querySelector('.navbar'), 'loaded');
+    }, 100); // 100ms de atraso para garantir que os elementos sejam ocultos inicialmente
+  }
 }
