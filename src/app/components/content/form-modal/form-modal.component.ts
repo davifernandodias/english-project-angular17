642602dc3a3244e841c6d2be811ef7479b83aecf
davi dias <davifernandosousadias18@gmail.com>
2024-09-05 21:31:01 -0300

    feat: added model form

---
+++ src/app/components/content/form-modal/form-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-modal.component.html',
+  styleUrls: ['./form-modal.component.scss']
+})
+export class FormModalComponent {
+}
